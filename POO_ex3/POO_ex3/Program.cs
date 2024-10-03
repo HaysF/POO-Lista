63b0bff7a337e8f341fe4387fca50609f22d0d5c
HaysF <105940611+HaysF@users.noreply.github.com>
2024-10-03 20:46:53 -0300

    Add files via upload

---
+++ POO_ex3/POO_ex3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrado que;
+            que = new Quadrado();
+
+            que.setBase(5);
+            que.setBase1(4);
+            que.calcularArea();
+            Console.WriteLine(que.getArea());
+        }
+    }
+}
---
+++ POO_ex3/POO_ex3/Quadrado.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_ex3
+{
+    internal class Quadrado
+    {
+        private int b;
+        private int b1;
+        private int area;
+        public void setBase(int b)
+        {
+            this.b = b;
+        }
+        public void setBase1(int b1)
+        {
+            this.b1 = b1;
+        }
+        public int getBase()
+        {
+            return this.b;
+        }
+        public int getAltura()
+        {
+            return this.b1;
+        }
+        public int getArea()
+        {
+            return this.area;
+        }
+        public void calcularArea()
+        {
+            this.area = (this.b * this.b1) / 2;
+        }
+    }
+}
