0e228404f4dd979ed4030b239ab7006e619fd8b1
HaysF <105940611+HaysF@users.noreply.github.com>
2024-10-03 20:36:56 -0300

    Add files via upload

---
+++ POO_ex2/projPOO_ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO_ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret;
+            ret = new Retangulo();
+
+            ret.setBase(6);
+            ret.setBase1(6);
+            ret.calcularArea();
+            Console.WriteLine(ret.getArea());
+        }
+    }
+}
